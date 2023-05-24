5462d71d1102d858769b3f9fdf06e1a2f902b4ca
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2023-05-24 13:27:55 +0100

    Update EventSource.js

--- www/EventSource.js
+++ www/EventSource.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'EventSource';
 
 exports.onOpen = function (url, success, error) {
-  exec(success, error, PLUGIN_NAME, "onOpen", [(serialnumber==''?null:url)]);
+  exec(success, error, PLUGIN_NAME, "onOpen", [url]);
 };
 
 exports.onMessage = function (success, error) {
