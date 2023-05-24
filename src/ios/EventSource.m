f8e0c74673d10d047187868664c55cb37e55a250
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2023-05-24 14:20:55 +0100

    Update EventSource.m

--- src/ios/EventSource.m
+++ src/ios/EventSource.m
@@ -1,29 +1,26 @@
 #import <EventSource/EventSource.h>
+#import <Cordova/CDVPlugin.h>
 
-@interface MyEventSource : NSObject
+@interface MyEventSource : CDVPlugin
 
 @property (nonatomic, strong) EventSource *eventSource;
 
-- (instancetype)initWithURL:(NSString *)url;
-- (void)open;
-- (void)onMessage;
+- (void)initialize:(CDVInvokedUrlCommand *)command;
+- (void)open:(CDVInvokedUrlCommand *)command;
+- (void)onMessage:(CDVInvokedUrlCommand *)command;
 
 @end
 
 @implementation MyEventSource
 
-- (void)initWithURL:(CDVInvokedUrlCommand *)command {
-    NSString* url = [command.arguments objectAtIndex:0] != (NSString *)[NSNull null] ? [command.arguments objectAtIndex:0] : nil;
-    self = [super init];
-    if (self) {
-        self.eventSource = [[EventSource alloc] initWithURL:url];
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
-    return self;
+- (void)initialize:(CDVInvokedUrlCommand *)command {
+    NSString *url = [command.arguments objectAtIndex:0] != (NSString *)[NSNull null] ? [command.arguments objectAtIndex:0] : nil;
+    self.eventSource = [[EventSource alloc] initWithURL:[NSURL URLWithString:url]];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
- - (void)open:(CDVInvokedUrlCommand *)command {
+- (void)open:(CDVInvokedUrlCommand *)command {
     [self.eventSource onOpen:^{
         NSLog(@"Connection opened");
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
@@ -31,12 +28,12 @@
     }];
 }
 
- - (void)onMessage:(CDVInvokedUrlCommand *)command {
+- (void)onMessage:(CDVInvokedUrlCommand *)command {
     [self.eventSource onMessage:^(Event *event) {
         if (event.data) {
             NSLog(@"Received message: %@", event.data);
-                CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:event.data];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:event.data];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         }
     }];
 }
